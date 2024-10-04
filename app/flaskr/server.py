80889763db9d6f0893479fe68483b0b88d562757
Giuseppe Michetti <gius.michetti@gmail.com>
2024-10-04 01:05:20 -0500

    debugging rate-limiter - try thisgit add .

--- app/flaskr/server.py
+++ app/flaskr/server.py
@@ -58,8 +58,7 @@ app.config['SECRET_KEY'] = SECRET_KEY
 limiter = Limiter(
     get_remote_address,
     app=app,
-    storage_uri="".join([RL_HOST, ":", RL_PORT, "//", RL_DB])
-)
+    storage_uri=f"mongodb://{RL_HOST}:{RL_PORT}/{RL_DB}")
 
 def hide_password(password):
     if password and isinstance(password, str):
